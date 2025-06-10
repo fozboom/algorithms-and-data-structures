1b3a3cbcdf435ce255d261b85ca35855742bf4b8
fozboom <fozboom@gmail.com>
2025-06-11 00:38:01 +0300

    Add restaurant customers problem solution in Python

---
+++ py_src/Sortings/restaurant_customers.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+events = []
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    events.append((a, 1))
+    events.append((b + 1, -1))
+
+events.sort()
+
+current = max_customers = 0
+for _, change in events:
+    current += change
+    if current > max_customers:
+        max_customers = current
+
+print(max_customers)
