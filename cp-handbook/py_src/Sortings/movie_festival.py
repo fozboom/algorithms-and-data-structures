fb2f6bb37ed25ca1350c9826c3d7b0b4e443aaab
fozboom <fozboom@gmail.com>
2025-06-14 01:44:17 +0300

    Add movie festival scheduling solution in Python

---
+++ cp-handbook/py_src/Sortings/movie_festival.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+
+movies = []
+for _ in range(n):
+    start, end = map(int, input().split())
+    movies.append((start, end))
+
+movies.sort(key=lambda x: x[1])
+
+count = 0
+
+curr_end_time = 0
+
+for start, end in movies:
+    if start >= curr_end_time:
+        count += 1
+        curr_end_time = end
+
+print(count)
