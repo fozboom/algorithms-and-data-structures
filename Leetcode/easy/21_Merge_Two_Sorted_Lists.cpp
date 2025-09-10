51ac728f6e32844471851a9d7b913a84a8ea74d9
fozboom <fozboom@gmail.com>
2025-09-10 10:53:54 +0300

    Add C++ solution for Merge Two Sorted Lists problem and update README

---
+++ Leetcode/easy/21_Merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,29 @@
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+   public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {  // NOLINT
+        ListNode* fake_head = new ListNode();
+        ListNode* curr = fake_head;
+
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val < list2->val) {
+                curr->next = list1;
+                list1 = list1->next;
+            } else {
+                curr->next = list2;
+                list2 = list2->next;
+            }
+            curr = curr->next;
+        }
+        curr->next = list1 != nullptr ? list1 : list2;
+        return fake_head->next;
+    }
+};
